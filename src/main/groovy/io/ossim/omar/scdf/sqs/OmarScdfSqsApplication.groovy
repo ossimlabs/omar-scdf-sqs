6d5cf677e8e5c10234f88d54b49ce0b5fbf7ee48
Chris Dowin <cdowin@radiantblue.com>
2017-06-13 14:03:23 -0400

    Syncing with most up to date code

--- src/main/groovy/io/ossim/omar/scdf/sqs/OmarScdfSqsApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/sqs/OmarScdfSqsApplication.groovy
@@ -2,10 +2,8 @@ package io.ossim.omar.scdf.sqs
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.cloud.aws.messaging.listener.Acknowledgment
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener
 import org.springframework.cloud.stream.annotation.EnableBinding
@@ -15,8 +13,13 @@ import org.springframework.messaging.handler.annotation.MessageMapping
 
 /**
  * Created by adrake on 5/31/2017
+ * Modified by cdowin on 6/5/2017
+ *
+ * The OmarScdfSqsApplication is a purpose built SQS listener
+ * for integration with a full SCDF stack. The SQS listener
+ * pops messages off of a configured queue and passes the message
+ * along to the next link in the SCDF chain.
  */
-
 @SpringBootApplication
 @EnableBinding(Source.class)
 class OmarScdfSqsApplication {
@@ -24,15 +27,18 @@ class OmarScdfSqsApplication {
 	/**
 	 * The application logger
 	 */
-	private Logger logger = LoggerFactory.getLogger(this.getClass())
+	private final Logger logger = LoggerFactory.getLogger(this.getClass())
 
-	BucketFile sqsMessageReceived
+    /**
+     * The AWS S3 bucket and file name
+     */
+    private String sqsMessage
 
 	/**
 	 * Constructor
 	 */
 	OmarScdfSqsApplication(){
-		sqsMessageReceived = new BucketFile()
+
 	}
 
 	/**
@@ -43,37 +49,27 @@ class OmarScdfSqsApplication {
 		SpringApplication.run OmarScdfSqsApplication, args
 	}
 
+    /**
+     * The callback for when an SQS message is received
+     * @param message the body of the SQS message from the queue
+     */
 	 @MessageMapping('${queue.name}')
-	 @SqsListener(value = '${queue.name}', deletionPolicy = SqsMessageDeletionPolicy.NEVER)
-	 void receive(String message, Acknowledgment acknowledgment) {
+	 @SqsListener(value = '${queue.name}', deletionPolicy = SqsMessageDeletionPolicy.ALWAYS)
+	 void receive(final String message) {
 
-		if(logger.isDebugEnabled()){
+		if(logger.isDebugEnabled()) {
 			logger.debug("Message received: ${message}")
 		}
 
-		def slurper = new groovy.json.JsonSlurper()
-		def result = slurper.parseText(message)
-
-
-		 sqsMessageReceived.bucket = result.Records.s3.bucket.name
-		 sqsMessageReceived.filename =  result.Records.s3.object.key
-
-		if(logger.isDebugEnabled()){
-			logger.debug("sqsMessageReceived: ${sqsMessageReceived}")
-		}
+         sqsMessage = message
 	}
 
+    /**
+     * The SCDF funtion to send parsed data long to the next piece in the chain
+     * @return the parsed JSON data
+     */
 	@InboundChannelAdapter(Source.OUTPUT)
-	BucketFile send() {
-        sqsMessageReceived
-	}
-
-	/**
-	 * @String filename output file name
-	 * @String bucket output bucket
-	 */
-	private final class BucketFile {
-		String filename
-		String bucket
+	final String send() {
+        sqsMessage
 	}
 }
