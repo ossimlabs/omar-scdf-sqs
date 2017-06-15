ad96417e84df25f0c01ed31f8ad111497eb0db8e
slallier <stephen.lallier@digitalglobe.com>
2017-06-14 20:15:52 -0400

    added message resolver to send a message on the output channel manually

--- src/main/groovy/io/ossim/omar/scdf/sqs/OmarScdfSqsApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/sqs/OmarScdfSqsApplication.groovy
@@ -1,14 +1,18 @@
 package io.ossim.omar.scdf.sqs
 
 import groovy.util.logging.Slf4j
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener
 import org.springframework.cloud.stream.annotation.EnableBinding
+import org.springframework.cloud.stream.binding.BinderAwareChannelResolver
 import org.springframework.cloud.stream.messaging.Source
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHeaders
 import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.messaging.support.MessageBuilder
 
 /**
  * Created by adrake on 5/31/2017
@@ -25,7 +29,13 @@ import org.springframework.messaging.handler.annotation.SendTo
 class OmarScdfSqsApplication
 {
 
-	/**
+    /**
+     * Output channel resolver to send messages on
+     */
+    @Autowired
+    private BinderAwareChannelResolver resolver
+
+    /**
 	 * Constructor
 	 */
 	OmarScdfSqsApplication() {}
@@ -43,11 +53,23 @@ class OmarScdfSqsApplication
 	 * The callback for when an SQS message is received
 	 * @param message the body of the SQS message from the queue
 	 */
+    @MessageMapping('${queue.name}')
 	@SqsListener(value = '${queue.name}', deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
-    @SendTo(Source.OUTPUT)
-	final String receiveFromSqsQueue(final String message)
+	void receiveFromSqsQueue(final String message)
 	{
 		log.debug("Forwarding message from queue: ${message}")
-        message
+        sendMessageOnOutputStream(message)
 	}
+
+    /**
+     * Sends message to an Output stream
+     */
+    void sendMessageOnOutputStream(String message)
+    {
+        Message<String> messageToSend = MessageBuilder.withPayload(message)
+                .setHeader(MessageHeaders.CONTENT_TYPE, '${spring.cloud.stream.bindings.output.content.type}')
+                .build()
+
+        resolver.resolveDestination(Source.OUTPUT).send(messageToSend)
+    }
 }
