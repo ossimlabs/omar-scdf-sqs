37a0934bbab49796ec5a43ac596ee1856ddad844
slallier <stephen.lallier@digitalglobe.com>
2017-06-15 10:15:06 -0400

    testing just using the @Output annotation to indicate output channel

--- src/main/groovy/io/ossim/omar/scdf/sqs/OmarScdfSqsApplication.groovy
+++ src/main/groovy/io/ossim/omar/scdf/sqs/OmarScdfSqsApplication.groovy
@@ -1,15 +1,15 @@
 package io.ossim.omar.scdf.sqs
 
 import groovy.util.logging.Slf4j
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy
 import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener
 import org.springframework.cloud.stream.annotation.EnableBinding
-import org.springframework.cloud.stream.binding.BinderAwareChannelResolver
+import org.springframework.cloud.stream.annotation.Output
 import org.springframework.cloud.stream.messaging.Source
 import org.springframework.messaging.Message
+import org.springframework.messaging.MessageChannel
 import org.springframework.messaging.MessageHeaders
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.messaging.support.MessageBuilder
@@ -28,12 +28,11 @@ import org.springframework.messaging.support.MessageBuilder
 @Slf4j
 class OmarScdfSqsApplication
 {
-
     /**
-     * Output channel resolver to send messages on
+     * Output channel
      */
-    @Autowired
-    private BinderAwareChannelResolver resolver
+    @Output(Source.OUTPUT)
+    private MessageChannel outputChannel
 
     /**
 	 * Constructor
@@ -70,6 +69,6 @@ class OmarScdfSqsApplication
                 .setHeader(MessageHeaders.CONTENT_TYPE, '${spring.cloud.stream.bindings.output.content.type}')
                 .build()
 
-        resolver.resolveDestination(Source.OUTPUT).send(messageToSend)
+        outputChannel.send(messageToSend)
     }
 }
